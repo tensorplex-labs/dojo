fdffab2e3b73e548cb8fbcc26b92ae880ecdbe84
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2025-06-04 18:03:35 +0800

    chore: remove old test code

--- dojo/messaging/client.py
+++ dojo/messaging/client.py
@@ -80,12 +80,18 @@ class Client:
             "accept-encoding": "zstd",
         }
 
-    async def _build_headers(self, include_compression: bool = True) -> dict[str, str]:
+    async def _build_headers(
+        self,
+        include_compression: bool = True,
+        message: str = None,  # type: ignore[assignment]
+    ) -> dict[str, str]:
         hotkey: str = self._wallet_info.hotkey
-        # TODO: replace meme message
-        message: str = f"I solemnly swear that I am up to some good. Hotkey: {hotkey}"
+        if not message:
+            message: str = (
+                f"I solemnly swear that I am up to some good. Hotkey: {hotkey}"
+            )
+
         signature: str = await self._kami.sign_message(message)
-        # TODO: use wallet nonce
         headers = {
             "content-type": "application/json",
             SIGNATURE_HEADER: signature,
@@ -333,73 +339,3 @@ class Client:
             await self._session.close()
         except Exception:
             pass
-
-
-# # NOTE: example usage below
-# async def main():
-#     import substrateinterface
-#
-#     # NOTE: example here to generate a valid signature, message, and hotkey
-#     keypair = substrateinterface.Keypair.create_from_uri("//Alice")
-#     message = "bingbong"
-#     signature = keypair.sign(message)
-#     logger.info(f"Signature: {signature}")
-#     logger.info(f"Hotkey: {keypair.ss58_address}")
-#     logger.info(f"message: {message}")
-#
-#     # TODO: this should be miner's axon IP
-#     url = "http://127.0.0.1:9001"
-#     session = get_client()
-#     wallet_info = WalletInfo(
-#         coldkey=keypair.ss58_address,
-#         hotkey=keypair.ss58_address,
-#         coldkey_path=Path("."),
-#         hotkey_path=Path("."),
-#     )
-#     client = Client(session=session, wallet_info=wallet_info)
-#     is_healthy = await client.health_check(url)
-#     logger.info(f"Server health check: {is_healthy}")
-#
-#     # # NOTE: testing SyntheticTaskSynapse
-#     # task_payload = SyntheticTaskSynapse(
-#     #     prompt="Hello",
-#     #     task_type=TaskTypeEnum.CODE_GENERATION,
-#     #     # completion_responses=[],
-#     #     completion_responses=[
-#     #         CompletionResponse(
-#     #             model="gpt-4",
-#     #             completion=CodeAnswer(files=[]),
-#     #             completion_id="123",
-#     #         )
-#     #     ],
-#     #     expire_at="2026-10-01T00:00:00Z",
-#     # )
-#     # json_data = task_payload.model_dump_json()
-#     # response, returned_payload = await client.send(url, model=task_payload)
-#     # compressed = encode_body(task_payload, client._build_headers())  # pyright: ignore
-#
-#     heartbeat = Heartbeat(ack=False)
-#     response, returned_payload = await client.send(url, model=heartbeat)
-#     json_data = heartbeat.model_dump_json()
-#     compressed = encode_body(heartbeat, await client._build_headers())
-#
-#     if response:
-#         logger.debug(f"Status: {response.status}")
-#         logger.debug(f"Original size: {len(json_data)} bytes")
-#         logger.debug(f"Compressed size: {len(compressed)} bytes")
-#         logger.debug(f"Compression ratio: {len(compressed) / len(json_data):.2f}")
-#
-#     if returned_payload:
-#         logger.debug(f"Returned payload: {returned_payload}")
-#         logger.debug(f"{returned_payload.body=}")
-#
-#     try:
-#         await client.cleanup()
-#         await session.close()
-#     except Exception:
-#         logger.warning("Exception occurred while cleaning up the client session")
-#         pass
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
