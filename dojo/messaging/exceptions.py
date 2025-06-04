607801aecb79f9b423952366a95c2dd6a9cd1bc3
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2025-06-04 19:16:28 +0800

    chore: remove dojo-messaging

--- dojo/messaging/exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidSignatureException(Exception):
-    """Exception raised when signature is invalid"""
-
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
