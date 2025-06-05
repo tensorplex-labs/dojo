d2c50a6a690a7b525cd48de14ccc4167596857d0
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2025-06-05 20:36:38 +0800

    chore: add fixme

--- neurons/miner.py
+++ neurons/miner.py
@@ -49,6 +49,7 @@ class Miner(aobject):
         await self.register_synapse_handlers()
         await self.init_metagraphs()
         # NOTE: keep track validator task id to dojo task id mapping
+        # FIXME: data persistence in redis-om
         self.vali_to_dojo_task_id: BoundedDict = BoundedDict(max_size=1000)
         # log all incoming requests
         self.hotkey_to_request: Dict[str, SyntheticTaskSynapse] = {}
